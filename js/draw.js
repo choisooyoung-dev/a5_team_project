d988389e84555630fe709970053f30fa78bf6803
한승준 <jk2tmdwns@naver.com>
2023-10-25 15:41:20 +0900

    feat : add clickevent

--- js/draw.js
+++ js/draw.js
@@ -29,5 +29,13 @@ export async function draw(moviesData) {
     </div>
     `;
     posterBox.innerHTML += addhtml;
+    const poster = document.getElementsByClassName("poster");
+    for (let i = 0; i < poster.length; i++) {
+      poster[i].addEventListener("click", () => {
+        const movie = poster[i];
+        const movieId = movie.dataset.id;
+        console.log(movieId);
+      });
+    }
   });
 }
