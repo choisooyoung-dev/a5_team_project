931ef9bb43c6eb4452fa16975b305e211ddf1408
choisooyoung-dev <sooyoung.choi.dev@gmail.com>
2023-10-27 18:06:26 +0900

    del: 윤겸님 테스트 코드 파일들 삭제

--- test/js/총 댓글 수표시.js
+++
@@ -1,8 +0,0 @@
-// h10 태그에 span요소 가져오기
-const COMMENT_TOTAL = document.querySelector("h10 > span");
-
-// 댓글의 총 개수를 표현하는 함수
-function Total() {
-    let comments = JSON.parse(localStorage.getItem('COMMENTLIST'));
-    COMMENT_TOTAL.innerText = comments.length;
-}
--- test/js/페이지로드시, 함수실행.js
+++
@@ -1,4 +0,0 @@
-// 페이지 로드시에 화면에 댓글 표시
-window.onload = function () {
-    DisplayComments();
-}
